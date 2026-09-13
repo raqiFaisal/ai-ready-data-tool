6ee9a5ed73cc088ee656b5d63cbe28b93575c901
Ruqia Faisal <unique0167@email.com>
2026-09-13 21:12:58 +0300

    feat: normalize column names

--- clean.py
+++ clean.py
@@ -5,3 +5,13 @@ def load_data(path):
 
 def handle_missing_values(data):
         return data.fillna(0)
+
+
+def normalize_columns(df):
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+    return df
